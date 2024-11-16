b0f368fca29c23584f2d2c2dbc3dc48e024b708f
DChen2024 <51212752+DChen2024@users.noreply.github.com>
2024-11-16 14:03:33 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# DChen2024.github.io
+## Website
+
+This is not meant to be a professional portfolio.
+
+Made this to become more familiar with GitHub. And with creating a website.
+
+Intended to be a fun pet project. :sweat_smile:
+
+Link to the website: https://dchen2024.github.io/
---
+++ script.js
@@ -0,0 +1,14 @@
+// Toggles between light mode and dark mode
+function toggleDarkMode(e) {
+  if (e.checked)
+    document.documentElement.style.setProperty("color-scheme", "dark");
+  else
+    document.documentElement.style.setProperty("color-scheme", "light");
+}
+
+// Initialize dark mode toggle state
+if (window.matchMedia('(prefers-color-scheme: dark)').matches)
+  document.getElementById("darkmode").checked = true;
+
+// Update the datetime every second
+setInterval(() => postMessage(Date()), 1000); // Date().slice(0, 24)
